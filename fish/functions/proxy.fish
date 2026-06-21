65ae2bb030ad330c316e6b329e698f78f182c4de
dev24hrs <asang24dev@gmail.com>
2026-06-21 21:11:02 +0800

    feat: update proxy

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -1,15 +1,15 @@
 function proxy --description 'proxy [on|off]'
     switch "$argv[1]"
         case on
-            set -x http_proxy http://127.0.0.1:7897
-            set -x https_proxy http://127.0.0.1:7897
-            set -x all_proxy socks5://127.0.0.1:7897
+            set -gx HTTP_PROXY http://127.0.0.1:7897
+            set -gx HTTPS_PROXY http://127.0.0.1:7897
+            set -gx ALL_PROXY socks5://127.0.0.1:7897
             echo (set_color green)"proxy on → 127.0.0.1:7897"(set_color normal)
 
         case off
-            set -e http_proxy
-            set -e https_proxy
-            set -e all_proxy
+            set -e HTTP_PROXY
+            set -e HTTPS_PROXY
+            set -e ALL_PROXY
             echo (set_color red)"proxy off"(set_color normal)
         case '*'
             echo "Usage: proxy [on|off]"
